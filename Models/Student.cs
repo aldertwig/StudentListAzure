dbeeb7099b370c0964c757fd89d5e3d98cbfbdb6
johan aldertwig <carl.johan.ahlqvist@outlook.com>
2025-01-23 01:13:38 +0100

    Display name

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,9 +1,15 @@
-﻿namespace StudentListMVC.Models
+﻿using System.ComponentModel;
+
+namespace StudentListMVC.Models
 {
     public class Student
     {
         public int Id { get; set; }
+
+        [DisplayName("First Name")]
         public string? Firstname { get; set; }
+
+        [DisplayName("Last Name")]
         public string? Lastname { get; set; }
     }
 }
